ae711f525ff1747555c432b4d0f039bc5698c1c5
RKRafaelNascimento <36175807+RKRafaelNascimento@users.noreply.github.com>
2025-03-03 10:23:19 -0300

    test: fix post unit test

--- src/modules/Post/__test__/Post.service.spec.ts
+++ src/modules/Post/__test__/Post.service.spec.ts
@@ -2,12 +2,14 @@ import { PostService } from "@/modules/Post";
 import { IPostRepository } from "@/modules/Post/interfaces";
 import { ISentimentAnalysisService } from "@/shared/sentimentAnalysisService/interfaces";
 import { TooManyRequestsError } from "@/shared/errors";
+import { IUserService } from "@/modules/User/interfaces";
 import { PostErrorCode } from "@/modules/Post/error";
 
 const MAX_DAILY_POSTS = 5;
 let postRepositoryMock: jest.Mocked<IPostRepository>;
 let sentimentAnalysisServiceMock: jest.Mocked<ISentimentAnalysisService>;
 let postService: PostService;
+let userServiceMock: jest.Mocked<IUserService>;
 
 const mockPost = {
   id: 1,
@@ -15,28 +17,38 @@ const mockPost = {
   content: "This is a test post",
   sentiment: "neutral" as "neutral" | "pos" | "neg",
   createdAt: new Date(),
+  getUserProfile: jest.fn(),
 };
 
 beforeAll(() => {
   postRepositoryMock = {
     create: jest.fn(),
-    countPostsTodayByUser: jest.fn(),
+    getById: jest.fn(),
+    getAllPostsWithReposts: jest.fn(),
+    getPostsAndRepostsFromFollowing: jest.fn(),
+    getPostsAndRepostByUserId: jest.fn(),
   };
 
   sentimentAnalysisServiceMock = {
     analyzeText: jest.fn(),
   };
+  userServiceMock = {
+    getById: jest.fn(),
+    countLimitedPostsAndRepostsTodayByUser: jest.fn(),
+    getUserProfile: jest.fn(),
+  };
 
   postService = new PostService(
     postRepositoryMock,
     sentimentAnalysisServiceMock,
+    userServiceMock,
   );
 });
 
 describe("PostService", () => {
   describe("Method: create", () => {
     it("Should throw TooManyRequestsError if user exceeds daily post limit", async () => {
-      postRepositoryMock.countPostsTodayByUser.mockResolvedValueOnce(
+      userServiceMock.countLimitedPostsAndRepostsTodayByUser.mockResolvedValueOnce(
         MAX_DAILY_POSTS,
       );
 
@@ -51,7 +63,9 @@ describe("PostService", () => {
     });
 
     it("Should successfully create a post when under the daily limit", async () => {
-      postRepositoryMock.countPostsTodayByUser.mockResolvedValueOnce(3);
+      userServiceMock.countLimitedPostsAndRepostsTodayByUser.mockResolvedValueOnce(
+        3,
+      );
       sentimentAnalysisServiceMock.analyzeText.mockResolvedValueOnce("neutral");
       postRepositoryMock.create.mockResolvedValueOnce(mockPost);
 
@@ -60,7 +74,9 @@ describe("PostService", () => {
         content: "Another test post",
       });
 
-      expect(postRepositoryMock.countPostsTodayByUser).toHaveBeenCalledWith(3);
+      expect(
+        userServiceMock.countLimitedPostsAndRepostsTodayByUser,
+      ).toHaveBeenCalledWith(3);
       expect(sentimentAnalysisServiceMock.analyzeText).toHaveBeenCalledWith(
         "Another test post",
       );
